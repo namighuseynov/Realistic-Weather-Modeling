a6aaedeffc8b17c72035120e6b240f6b42b4e0f9
Namig Huseynov <94350345+namighuseynov@users.noreply.github.com>
2024-03-29 12:53:11 +0400

    Time Flowing

--- Realistic weather modeling/Assets/Scripts/Clock.cs
+++ Realistic weather modeling/Assets/Scripts/Clock.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace RealisticWeatherModeling
 {
     namespace Time
@@ -12,12 +14,23 @@ namespace RealisticWeatherModeling
             private int _initialHour = 0;
             private int _initialMinute = 0;
             private int _initialSecond = 0;
+            private float _elapsedTime = 0;
 
             public Clock(int hour, int minute, int second)
             {
                 _initialHour = hour;
                 _initialMinute = minute;
                 _initialSecond = second;
+                _elapsedTime = (_initialSecond * 60) + (_initialMinute * 60 * 60) + (_initialHour * 60 * 60 * 60);
+            }
+
+            public void Update(float deltaTime)
+            {
+                _elapsedTime += deltaTime;
+                Seconds = Mathf.FloorToInt(_elapsedTime % 60);
+                Minute = Mathf.FloorToInt((_elapsedTime / 60) % 60);
+                Hour = Mathf.FloorToInt((_elapsedTime / 3600) % 24);
+
             }
 
             public void Update(int elapsedHour, int elapsedMinute, int elapsedSecond)
--- Realistic weather modeling/Assets/Scripts/Managers/TimeManager.cs
+++ Realistic weather modeling/Assets/Scripts/Managers/TimeManager.cs
@@ -8,9 +8,11 @@ namespace RealisticWeatherModeling
         public class TimeManager : Manager
         {
             [SerializeField] private Clock _clock;
-            private float _elapsedTime;
+            [SerializeField] private float _elapsedTime;
             [SerializeField] private float _timeScale = 1;
             [SerializeField] private int _elapsedDay;
+            [SerializeField] private Transform _sun;
+            [SerializeField] private bool _timeFlowing;
             public AnimationCurve curve;
             public Clock Clock { get; private set; }
             public float TimeScale { get { return _timeScale; } private set { } }
@@ -26,23 +28,44 @@ namespace RealisticWeatherModeling
             private void FixedUpdate()
             {
                 float deltaTime = UnityEngine.Time.deltaTime * TimeScale;
-                _elapsedTime += deltaTime;
-
-                int _seconds = Mathf.FloorToInt(_elapsedTime % 60);
-                int _minutes = Mathf.FloorToInt((_elapsedTime / 60) % 60);
-                int _hours = Mathf.FloorToInt((_elapsedTime / 3600) % 24);
-                int _day = Mathf.FloorToInt((_elapsedTime / 86400));
-                if (_day == 1)
+                if (_timeFlowing)
                 {
-                    _elapsedDay++;
-                    _elapsedTime = 0;
+                    _clock.Update(deltaTime);
+                    DayUpdate();
                 }
-
-                float deltaAngle = deltaTime * 360 / 86400;
-                //_sun.transform.Rotate(deltaAngle, 0, 0);
-                _clock.Update(_hours, _minutes, _seconds);
-                Debug.Log("here");
             }
+
+            private void DayUpdate()
+            {
+                float time = _clock.Hour + ((_clock.Minute * 5.0f / 3.0f) / 100.0f);
+                if (time < 6.5) time += 24;
+                float value = time / 30.5f;
+                float solarEulerAngle = curve.Evaluate(value) * 360.0f;
+                Quaternion rot = Quaternion.Euler(new Vector3(solarEulerAngle, 0, 0));
+                _sun.rotation = rot;
+                Debug.Log(solarEulerAngle);
+            }
+
+            //private void FixedUpdate()
+            //{
+            //    float deltaTime = UnityEngine.Time.deltaTime * TimeScale;
+            //    _elapsedTime += deltaTime;
+
+            //    int _seconds = Mathf.FloorToInt(_elapsedTime % 60);
+            //    int _minutes = Mathf.FloorToInt((_elapsedTime / 60) % 60);
+            //    int _hours = Mathf.FloorToInt((_elapsedTime / 3600) % 24);
+            //    int _day = Mathf.FloorToInt((_elapsedTime / 86400));
+            //    if (_day == 1)
+            //    {
+            //        _elapsedDay++;
+            //        _elapsedTime = 0;
+            //    }
+
+            //    float deltaAngle = deltaTime * 360 / 86400;
+            //    //_sun.transform.Rotate(deltaAngle, 0, 0);
+            //    _clock.Update(_hours, _minutes, _seconds);
+            //    Debug.Log("here");
+            //}
         }
     }
 }
